5b8152e1aea8d8577fc391eb9622c0998f20e15c
Jeff Kazuk <jeffreykazuk@gmail.com>
2024-04-13 18:39:09 -0500

    Morphed player into working with main world

--- Scenes/Spawners/Tree Spawner/tree_spawner.gd
+++ Scenes/Spawners/Tree Spawner/tree_spawner.gd
@@ -1,14 +1,12 @@
 @tool
 extends Node3D
 
-var trees:Array[PackedScene] = [preload("res://Assets/Trees/Tree3/Tree3.tscn")]
-var tree:Node3D = null
-
+var trees: Array[PackedScene] = [ preload ("res://Assets/Trees/Tree3/Tree3.tscn")]
+var tree: Node3D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -8,10 +8,11 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var distortion: float = 1.0
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera3d
-@onready var shader = get_tree().root.get_node("CanvasLayer/PostProcessing").get_material()
+# @onready var shader = get_tree().root.get_node("CanvasLayer/PostProcessing").get_material()
 
 func looking_at_mannequin() -> bool:
-	return get_parent().get_node("mannequin").get_node("VisibleOnScreenNotifier3D").is_on_screen()
+	# return get_parent().get_node("mannequin").get_node("VisibleOnScreenNotifier3D").is_on_screen()
+	return false
 
 func can_move() -> bool:
 	return !looking_at_mannequin()
@@ -28,10 +29,11 @@ func _unhandled_input(event: InputEvent) -> void:
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad( - 30), deg_to_rad(60))
 
 func _process(delta: float) -> void:
-	distortion = clamp(10.0 - global_position.distance_to(get_parent().get_node("mannequin").global_position), 1.0, 10.0)
-	shader.set_shader_parameter("distortion", distortion)
-	shader.set_shader_parameter("low_distortion", remap(distortion, 1., 10., 1., 2.))
-	shader.set_shader_parameter("high_distortion", remap(distortion, 1., 10., 1., 100.))
+	pass
+	# distortion = clamp(10.0 - global_position.distance_to(get_parent().get_node("mannequin").global_position), 1.0, 10.0)
+	# shader.set_shader_parameter("distortion", distortion)
+	# shader.set_shader_parameter("low_distortion", remap(distortion, 1., 10., 1., 2.))
+	# shader.set_shader_parameter("high_distortion", remap(distortion, 1., 10., 1., 100.))
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
