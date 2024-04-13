1bfe9fa25670487289e4011b12a6934b3eb1f488
Shane <shanescott86@gmail.com>
2024-04-13 08:42:02 -0500

    fixed wild neck

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,24 +1,39 @@
 extends CharacterBody3D
 
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-var mouse_sens = 0.3
-var camera_anglev = 0
+# Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var neck := $Neck
+@onready var camera := $Neck/Camera3d
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			neck.rotate_y(-event.relative.x * 0.01)
+			camera.rotate_x(-event.relative.y * 0.01)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y -= gravity * delta
 
-	# Handle jump.
+	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -27,11 +42,3 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-
-func _input(event) -> void:
-	if event is InputEventMouseMotion:
-		%camera.rotate_y(deg_to_rad( - event.relative.x * mouse_sens))
-		var changev = -event.relative.y * mouse_sens
-		if camera_anglev + changev > - 50 and camera_anglev + changev < 50:
-			camera_anglev += changev
-			%camera.rotate_x(deg_to_rad(changev))
