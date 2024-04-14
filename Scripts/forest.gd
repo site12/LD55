e48a7053a44efabcef977c56270c2d2a09060bf4
Jeff Kazuk <jeffreykazuk@gmail.com>
2024-04-13 19:27:40 -0500

    redid mannequin encounter to be in-world

--- Scripts/forest.gd
+++ Scripts/forest.gd
@@ -1,7 +1,5 @@
 extends Node3D
 
-const FLOOR_SCENE = preload ("res://Scenes/Dev/upstairs_floor.tscn")
-
 @onready var player = get_node("Player")
 
 # Called when the node enters the scene tree for the first time.
@@ -9,10 +7,10 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if player.looking_at_mannequin():
 
 		# Get the input direction and handle the movement/deceleration.
@@ -20,8 +18,8 @@ func _physics_process(delta: float) -> void:
 		var input_dir := Input.get_vector("left", "right", "forward", "backward")
 		input_dir = -input_dir
 		var direction = (player.neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		$floors.position += direction * .1
-		if (abs(snapped($floors.position.x, 0.1)) == 10.0):
-			$floors.position.x = 0.
-		if (abs(snapped($floors.position.z, 0.1)) == 18.0):
-			$floors.position.z = 0.
+		%floors.position += direction * .1
+		if (abs(snapped( %floors.position.x, 0.1)) == 20.0):
+			%floors.position.x = 0.
+		if (abs(snapped( %floors.position.z, 0.1)) == 20.0):
+			%floors.position.z = 0.
