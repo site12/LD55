f894a4b2406a6cb8fa2ab6526a65e7ed6377da74
Jeff Kazuk <jeffreykazuk@gmail.com>
2024-04-13 23:08:08 -0500

    Player can function in levels without mannequin

--- Scripts/forest.gd
+++ Scripts/forest.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @onready var player = get_node("Player")
 
+const MANNEQUIN_PRESENT = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
