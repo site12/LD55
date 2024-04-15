51c514b3ac99c46f0637a695a239bfc124d648d0
Shane <shanescott86@gmail.com>
2024-04-15 10:16:56 -0500

    breathing ;ppl ficls

--- Assets/mass/mass.gd
+++ Assets/mass/mass.gd
@@ -3,7 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimationPlayer.play("breateh")
+	%mass_anims.play("breateh")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
