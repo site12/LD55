5dea77a775d88d9f3f2da44a29ee93943af6deda
Shane <shanescott86@gmail.com>
2024-04-14 23:26:57 -0500

    footsteps are different based on surface

--- Scenes/Player/footsteps.gd
+++ Scenes/Player/footsteps.gd
@@ -9,7 +9,13 @@ func _ready():
 
 func play_sound():
 	var audio_player :AudioStreamPlayer2D = AudioStreamPlayer2D.new()
-	audio_player.stream = footstep_sounds[0]
+	print(%footstep_raycast.get_collider().get_parent().name)
+	if %footstep_raycast.get_collider().get_parent().name == "Default Layer":
+		audio_player.stream = footstep_sounds[0]
+	elif %footstep_raycast.get_collider().get_parent().name == "path" || %footstep_raycast.get_collider().get_parent().name == "basement":
+		audio_player.stream = footstep_sounds[1]
+	else:
+		audio_player.stream = footstep_sounds[2]
 
 	audio_player.pitch_scale = randf_range(0.8,1.1)
 	get_tree().get_root().add_child(audio_player)
