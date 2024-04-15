ed90299480276d5bd3b14d932e7383ae010f2130
Shane <shanescott86@gmail.com>
2024-04-15 00:07:24 -0500

    stuff

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -105,9 +105,7 @@ func _physics_process(delta: float) -> void:
 	camera.transform.origin = _headbob(t_bob)
 	
 	if can_move():
-		# Handle Jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
+
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
